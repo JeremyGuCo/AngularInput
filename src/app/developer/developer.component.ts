f111de1d9cf63729eabb2299f870bd3d0c3251ee
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-04-08 13:44:05 +0200

    Modif Skills

---
+++ src/app/developer/developer.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Developer } from '../common/developer.model'
+import { Skill } from '../common/skill.model'
+
+
+@Component({
+  selector: 'app-developer',
+  templateUrl: './developer.component.html',
+  styleUrls: ['./developer.component.css']
+})
+export class DeveloperComponent implements OnInit {
+public developer = new Developer('Jérémy','Guérin-Coquereau',31,'Homme','Ceci est une bio',[
+  new Skill ('Name','Logo','Site'),
+  new Skill ('JS', 'Javascript', 'Angular'),
+  new Skill ('CSS', 'HTML', 'Node.js'),
+]);
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+}
